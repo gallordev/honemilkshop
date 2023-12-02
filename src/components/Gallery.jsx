e82315e700e11bcf08c3ebae4bdd42742d1798b6
Oswaldo Gallo <gallor.dev@gmail.com>
2023-12-02 04:26:51 -0600

    wea

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -26,8 +26,8 @@ export default function Gallery() {
         <div className="container mx-auto p-4">
             {data.length > 0 && data.map(gallery => (
                 <div key={gallery.id} className="bg-gray-50 border border-gray-200 rounded-lg p-8 md:p-12 mb-8">
-                    <h1 className="text-gray-900 text-3xl md:text-5xl font-extrabold mb-2">{gallery.name}</h1>
-                    <ImageList imageURLs={gallery.images} useLinks={true} />
+                    <h1 key={gallery.id} className="text-gray-900 text-3xl md:text-5xl font-extrabold mb-2">{gallery.name}</h1>
+                    <ImageList key={gallery.id} imageURLs={gallery.images} useLinks={true} />
                 </div>
             ))}
         </div>
